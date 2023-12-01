cc4c3d4a435cd04573a145147fe9e0acbd63e74e
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-12-01 14:58:53 -0500

    add user-agent to input download

--- 2023/lib/input.ts
+++ 2023/lib/input.ts
@@ -6,7 +6,8 @@ export const getInput = async (day: number): Promise<string> => {
     }
     const text = await fetch(`https://adventofcode.com/2023/day/${day}/input`, {
         headers: {
-            cookie: `session=${process.env.SESSION_TOKEN}`
+            'User-Agent': 'github.com/zacharycoulter/advent-of-code',
+            'Cookie': `session=${process.env.SESSION_TOKEN}`
         }
     }).then((res: Response): Promise<string> => res.text());
     await Bun.write(inputFilePath, text);
