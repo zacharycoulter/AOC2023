c47a0999ffedf518711e8f495f08ecba1e2a4708
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-12-01 11:16:13 -0500

    clean up and make human friendly functions

--- days/1.ts
+++ days/1.ts
@@ -1,18 +1,23 @@
-const run = (part: number) =>
-    fetch(`https://adventofcode.com/2023/day/1/input`, {
+const getSumOfFirstLast = async (part: 1 | 2): Promise<number> => {
+    const addDigitalToNumWords = (lines: string[]): string[] => {
+        const numWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        return lines.map((line: string): string => numWords.reduce((acc: string, word: string, index: number): string => acc.replaceAll(word, word + (index + 1) + word), line))
+    }
+    const getSumOfLines = (lines: string[]): number => lines
+        .map((line: string): string[] => line.match(/\d/g) ?? [])
+        .reduce((acc: number, nums: string[]): number => acc += parseInt(nums[0] + nums[nums.length - 1]), 0)
+
+    return fetch(`https://adventofcode.com/2023/day/1/input`, {
         headers: {
             cookie: `session=${process.env.SESSION_TOKEN}`
         }
     })
-        .then((res) => res.text())
-        .then((text: string) => text.match(/(\S*)\n/g) as string[])
-        .then((lines: string[]) => part === 1
-            ? lines
-            : lines.map((line: string) => ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].reduce((acc: string, word: string, index: number) => acc.replaceAll(word, word + (index + 1) + word), line)))
-        .then((lines: string[]) => lines
-            .map((line: string) => line.match(/\d/g) as string[])
-            .reduce((acc: number, nums: string[]) => acc += parseInt(nums[0] + nums[nums.length - 1]), 0))
+        .then((res: Response): Promise<string> => res.text())
+        .then((text: string): string[] => text.split('\n').filter((line: string): boolean => line.length > 0))
+        .then((lines: string[]): string[] => part === 1 ? lines : addDigitalToNumWords(lines))
+        .then(getSumOfLines)
+}
 
 Promise
-    .all([run(1), run(2)])
+    .all([getSumOfFirstLast(1), getSumOfFirstLast(2)])
     .then(console.log)
