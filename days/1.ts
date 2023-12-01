f5ded1c7f9cf76476b540c907235dd11db8375b5
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-12-01 11:04:56 -0500

    add both parts

--- days/1.ts
+++ days/1.ts
@@ -1,12 +1,18 @@
-const result = fetch(`https://adventofcode.com/2023/day/1/input`, {
-    headers: {
-        cookie: `session=${process.env.SESSION_TOKEN}`
-    }
-})
-    .then((res) => res.text())
-    .then((text: string) => text.match(/(\S*)\n/g) as string[])
-    .then((lines: string[]) => lines
-        .map((line: string) => ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].reduce((acc: string, word: string, index: number) => acc.replaceAll(word, word + (index + 1) + word), line))
-        .map((line: string) => line.match(/\d/g) as string[])
-        .reduce((acc: number, nums: string[]) => acc += parseInt(nums[0] + nums[nums.length - 1]), 0))
+const run = (part: number) =>
+    fetch(`https://adventofcode.com/2023/day/1/input`, {
+        headers: {
+            cookie: `session=${process.env.SESSION_TOKEN}`
+        }
+    })
+        .then((res) => res.text())
+        .then((text: string) => text.match(/(\S*)\n/g) as string[])
+        .then((lines: string[]) => part === 1
+            ? lines
+            : lines.map((line: string) => ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].reduce((acc: string, word: string, index: number) => acc.replaceAll(word, word + (index + 1) + word), line)))
+        .then((lines: string[]) => lines
+            .map((line: string) => line.match(/\d/g) as string[])
+            .reduce((acc: number, nums: string[]) => acc += parseInt(nums[0] + nums[nums.length - 1]), 0))
+
+Promise
+    .all([run(1), run(2)])
     .then(console.log)
