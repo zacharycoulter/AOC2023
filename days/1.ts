4d3dc438ee39ee9ea3b2ae278230019766ae7ab0
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-12-01 01:50:09 -0500

    remove unneed async func wrapper

--- days/1.ts
+++ days/1.ts
@@ -1,14 +1,12 @@
-(async () => {
-    const result = (await fetch(`https://adventofcode.com/2023/day/1/input`, {
-        headers: {
-            cookie: `session=${process.env.SESSION_TOKEN}`
-        }
-    })
-        .then((res) => res.text())
-        .then((text: string) => text.match(/(\S*)\n/g) as string[]))
+const result = fetch(`https://adventofcode.com/2023/day/1/input`, {
+    headers: {
+        cookie: `session=${process.env.SESSION_TOKEN}`
+    }
+})
+    .then((res) => res.text())
+    .then((text: string) => text.match(/(\S*)\n/g) as string[])
+    .then((lines: string[]) => lines
         .map((line: string) => ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].reduce((acc: string, word: string, index: number) => acc.replaceAll(word, word + (index + 1) + word), line))
         .map((line: string) => line.match(/\d/g) as string[])
-        .reduce((acc: number, nums: string[]) => acc += parseInt(nums[0] + nums[nums.length - 1]), 0)
-
-    console.log(result)
-})()
+        .reduce((acc: number, nums: string[]) => acc += parseInt(nums[0] + nums[nums.length - 1]), 0))
+    .then(console.log)
