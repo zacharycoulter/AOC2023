74a3369ed4bae454490ca3fed76d04254b687043
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-12-02 00:57:33 -0500

    fix day 1 tests

--- 2023/days/01.test.ts
+++ 2023/days/01.test.ts
@@ -1,21 +1,23 @@
 import { expect, test } from "bun:test";
 import { part1, part2 } from '@days/01'
 
-test('day 1, part 1', async () => {
-    const input = Promise.resolve(`1abc2
+test('day 1, part 1', () => {
+    const input = `1abc2
         pqr3stu8vwx
         a1b2c3d4e5f
-        treb7uchet`)
-    expect(await part1(input, 1)).toBe(142)
+        treb7uchet
+`
+    expect(part1(input)).toBe(142)
 })
 
-test('day 1, part 2', async () => {
-    const input = Promise.resolve(`two1nine
+test('day 1, part 2', () => {
+    const input = `two1nine
         eightwothree
         abcone2threexyz
         xtwone3four
         4nineeightseven2
         zoneight234
-        7pqrstsixteen`)
-    expect(await part2(input, 2)).toBe(281)
+        7pqrstsixteen
+`
+    expect(part2(input)).toBe(281)
 })
